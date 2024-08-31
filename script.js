41465cc99020cbfdf9016042d441bb342137c58b
Luan Cristian <162592506+luanzura@users.noreply.github.com>
2024-08-31 14:59:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,78 @@
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Shop-Code": "648669",
-  },
-  body: '{"filter":{"stockControl":true,"page":1,"perPage":500,"category":"","subCategory":"","search":null,"sort":{"ProductName":1},"onlyPromo":false}}',
-};
-
+const perPage = 200; // Máximo permitido pela API
 let productList = [];
+let currentPage = 1;
+let hasMorePages = true;
 
-fetch("https://api.ecommerce.nextar.com/prod/api/products", options)
-  .then((response) => response.json())
-  .then((data) => {
-    productList = data.list; // Salvar a lista de produtos
-    displayProducts(productList); // Exibir produtos inicialmente
-  })
-  .catch((err) => console.error(err));
+function fetchAllProducts() {
+  if (!hasMorePages) {
+    displayProducts(productList);
+    return;
+  }
+
+  fetch(
+    `https://proxy-nuvem.vercel.app/products?page=${currentPage}&per_page=${perPage}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Verifique a estrutura da resposta aqui
+      if (Array.isArray(data)) {
+        productList = productList.concat(
+          data.map((product) => ({
+            name: product.name.pt,
+            image: product.images[0].src,
+          }))
+        );
+
+        // Se a resposta tem menos itens do que per_page, é a última página
+        hasMorePages = data.length === perPage;
+        currentPage++;
+
+        // Recursivamente buscar a próxima página
+        fetchAllProducts();
+      } else {
+        console.error("Unexpected data format:", data);
+        hasMorePages = false;
+      }
+    })
+    .catch((err) => console.error("Fetch error:", err));
+}
 
 function displayProducts(products) {
   const container = document.getElementById("product-container");
   container.innerHTML = ""; // Limpar o container antes de exibir os produtos
 
   products.forEach((product) => {
-    // Extrair as informações necessárias
-    const productCode = product.ProductCode;
-    const productName = product.ProductName;
-    const productImg = product.ProductImg;
+    const productName = product.name;
+    const productImg = product.image;
 
-    // Construir a URL da imagem usando a base fornecida e o ProductImg específico
-    const imgUrl = `https://storage.googleapis.com/nexapp-flutter.appspot.com/production/products/${productImg}`;
-
-    // Criar elementos HTML
     const productDiv = document.createElement("div");
     productDiv.classList.add("col-md-4", "product");
 
     const img = document.createElement("img");
-    img.src = imgUrl;
+    img.src = productImg;
     img.alt = productName;
 
-    // Adicionar evento de clique na imagem para abrir a imagem em uma nova aba
     img.addEventListener("click", () => {
-      window.open(imgUrl, "_blank");
+      window.open(productImg, "_blank");
     });
 
     const namePara = document.createElement("p");
     namePara.textContent = productName;
 
-    const codePara = document.createElement("p");
-    codePara.textContent = `Código do produto: ${productCode}`;
-
-    // Adicionar elementos ao div do produto
     productDiv.appendChild(img);
     productDiv.appendChild(namePara);
-    productDiv.appendChild(codePara);
 
-    // Adicionar o div do produto ao container no HTML
     container.appendChild(productDiv);
   });
 }
 
-// Adicionar evento de input no campo de pesquisa
 document.getElementById("search-input").addEventListener("input", (event) => {
   const searchTerm = event.target.value.toLowerCase();
   const filteredProducts = productList.filter((product) =>
-    product.ProductName.toLowerCase().includes(searchTerm)
+    product.name.toLowerCase().includes(searchTerm)
   );
-  displayProducts(filteredProducts); // Exibir produtos filtrados
+  displayProducts(filteredProducts);
 });
+
+// Inicie a busca de produtos
+fetchAllProducts();
