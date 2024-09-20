52ee63ffd5d7b6c71a7fee25efc537088281a26e
Luan Cristian <162592506+luanzura@users.noreply.github.com>
2024-09-20 10:04:57 -0300

    Update script_bones.js

--- script_bones.js
+++ script_bones.js
@@ -248,3 +248,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const triggerTabList = [].slice.call(
+  document.querySelectorAll("#category-tabs a")
+);
+triggerTabList.forEach((triggerEl) => {
+  const tabTrigger = new bootstrap.Tab(triggerEl);
+
+  triggerEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    tabTrigger.show();
+  });
+});
